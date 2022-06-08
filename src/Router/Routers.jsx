7885f88eea014c1de4ddbd38afa7e4a909a55f15
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-06-08 16:32:45 +0530

    feat : created require auth added to private routes

--- src/Router/Routers.jsx
+++ src/Router/Routers.jsx
@@ -2,16 +2,45 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MockmanEs from "mockman-js";
 import { Archive, Home, Labels, Login, Notes, Signup, Trash } from "../Pages";
+import { RequireAuth } from "../RequireAuth";
 export const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/labels" element={<Labels />} />
-      <Route path="archive" element={<Archive />} />
-      <Route path="/trash" element={<Trash />} />
-      <Route path="/notes" element={<Notes />} />
+      <Route
+        path="/labels"
+        element={
+          <RequireAuth>
+            <Labels />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="archive"
+        element={
+          <RequireAuth>
+            <Archive />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/trash"
+        element={
+          <RequireAuth>
+            <Trash />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/notes"
+        element={
+          <RequireAuth>
+            <Notes />
+          </RequireAuth>
+        }
+      />
       <Route path="/mock" element={<MockmanEs />} />
     </Routes>
   );
